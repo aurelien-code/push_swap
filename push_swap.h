53ca528934ba114aebf429bc6cfab2e288674049
aumarin <aumarin@student.42.fr>
2022-07-25 15:54:32 +0200

    refactor: moves directly affect the stack

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 22:42:30 by aumarin           #+#    #+#             */
-/*   Updated: 2022/06/17 16:18:54 by aumarin          ###   ########.fr       */
+/*   Updated: 2022/07/25 15:53:06 by aumarin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,11 +57,12 @@ int		ft_lstsize(t_stack *lst);
 int		is_argv_valid(int argc, char **argv);
 t_stack	*init_stack(int size, char **numbers);
 void	print_stack(t_stack *lst);
-t_stack	*swap(t_stack *head, char move);
-t_stack	*rotate(t_stack *head);
-t_stack	*reverse_rotate(t_stack *head);
-t_stack	*push(t_stack **origin, t_stack **dest, char move);
 int		is_stack_sorted(t_stack *stack);
 void	sort_small_stack(t_stack *stack, t_stack *stack_b);
+
+void	swap(t_stack **stack, char move);
+void	rotate(t_stack	**stack, char move);
+void	reverse_rotate(t_stack **stack, char move);
+void	push(t_stack **origin, t_stack **dest, char move);
 
 #endif
--- srcs/moves.c
+++ srcs/moves.c
@@ -6,54 +6,59 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 17:03:55 by aumarin           #+#    #+#             */
-/*   Updated: 2022/06/17 16:18:46 by aumarin          ###   ########.fr       */
+/*   Updated: 2022/07/25 15:53:17 by aumarin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*swap(t_stack *head, char move)
-{
-	t_stack	*second;
-
-	second = head->next;
-	head->next = second->next;
-	second->next = head;
-	ft_printf("s%c\n", move);
-	return (second);
-}
-
-t_stack	*rotate(t_stack *head)
-{
-	t_stack	*last;
-	t_stack	*second;
-
-	second = head->next;
-	last = ft_lstlast(head);
-	last->next = head;
-	head->next = NULL;
-	return (second);
-}
-
-t_stack	*reverse_rotate(t_stack *head)
-{
-	t_stack	*last;
-
-	last = ft_lstlast(head);
-	last->next = head;
-	while (head->next != last)
-		head = head->next;
-	head->next = NULL;
-	return (last);
-}
-
-t_stack	*push(t_stack **origin, t_stack **dest, char move)
+void	swap(t_stack **stack, char move)
 {
 	t_stack	*tmp;
 
 	tmp = malloc(sizeof(t_stack));
 	if (!tmp)
-		return (NULL);
+		return ;
+	tmp->value = (*stack)->value;
+	tmp->next = (*stack)->next->next;
+	*stack = (*stack)->next;
+	(*stack)->next = tmp;
+	ft_printf("s%c\n", move);
+}
+
+void	rotate(t_stack	**stack, char move)
+{
+	t_stack	*last;
+	t_stack	*second;
+
+	last = ft_lstlast(*stack);
+	second = (*stack)->next;
+	(*stack)->next = NULL;
+	last->next = *stack;
+	(*stack) = second;
+	ft_printf("r%c\n", move);
+}
+
+void	reverse_rotate(t_stack **stack, char move)
+{
+	t_stack	*last;
+
+	last = ft_lstlast(*stack);
+	last->next = *stack;
+	while ((*stack)->next != last)
+		*stack = (*stack)->next;
+	(*stack)->next = NULL;
+	(*stack) = last;
+	ft_printf("rr%c\n", move);
+}
+
+void	push(t_stack **origin, t_stack **dest, char move)
+{
+	t_stack	*tmp;
+
+	tmp = malloc(sizeof(t_stack));
+	if (!tmp)
+		return ;
 	tmp->value = origin[0]->value;
 	if (*dest)
 		tmp->next = *dest;
@@ -65,5 +70,4 @@ t_stack	*push(t_stack **origin, t_stack **dest, char move)
 	else
 		*origin = NULL;
 	ft_printf("p%c\n", move);
-	return (*dest);
 }
