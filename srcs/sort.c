1ca46dd7cb9a6e43acf0e1ef201e5e09e79aaa75
aumarin <aumarin@student.42.fr>
2022-07-27 02:30:11 +0200

    add: function to know if top value is the minimum value

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 02:29:39 by aumarin           #+#    #+#             */
-/*   Updated: 2022/07/26 00:37:30 by aumarin          ###   ########.fr       */
+/*   Updated: 2022/07/26 19:14:44 by aumarin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,28 +50,37 @@ t_stack	*sort_small_stack(t_stack *stack_a)
 	return (stack_a);
 }
 
+int	is_top_minimum(t_stack *stack)
+{
+	int	min;
+
+	min = stack->value;
+	while (stack->next)
+	{
+		if (min > stack->value)
+			return (0);
+		else
+			stack = stack->next;
+	}
+	ft_printf("MINIMUM -> %d\n", min);
+	return (1);
+}
+
 void	sort_medium_stack(t_stack *stack_a, t_stack *stack_b)
 {
 	while (ft_lstsize(stack_a) > 3)
-		push(&stack_a, &stack_b, 'b');
+	{
+		if (is_top_minimum(stack_a))
+			push(&stack_a, &stack_b, 'b');
+		else
+			reverse_rotate(&stack_a, 'a');
+	}
+	ft_printf("STACKB\n");
+	print_stack(stack_b);
 	stack_a = sort_small_stack(stack_a);
 	while (ft_lstsize(stack_b) > 1)
 	{
-		if (stack_b->value < stack_a->value)
-			push(&stack_b, &stack_a, 'a');
-		else
-		{
-			if (stack_b->value > ft_lstlast(stack_a)->value)
-			{
-				push(&stack_b, &stack_a, 'a');
-				rotate(&stack_a, 'a');
-			}
-			while (stack_a->value < stack_b->value)
-				reverse_rotate(&stack_a, 'a');
-			push(&stack_b, &stack_a, 'a');
-			while (!is_stack_sorted(stack_a))
-				rotate(&stack_a, 'a');
-		}
+		push(&stack_b, &stack_a, 'a');
 	}
 	print_stack(stack_a);
 }
